bb899b5e933fd24c51587fdbdb36ef8c3af24b73
yzdann <1543472-yzdann@users.noreply.gitlab.com>
2019-11-29 15:41:58 +0330

    add python file that contain list of snmp samples output

---
+++ snmp_data_samples.py
@@ -0,0 +1,43 @@
+# most of data collected from
+# https://github.com/CiscoTestAutomation/genieparser
+# get_platform and tests modules
+snmp_samples_list = [
+  """Cisco IOS Software [Everest], Catalyst L3 Switch Software (CAT3K_CAA-UNIVERSALK9-M), Experimental Version 16.4.20170410:165034 [v164_throttle-BLD-BLD_V164_THROTTLE_LATEST_20170410_174845 105]
+  Copyright (c) 1986-2017 by Cisco Systems, Inc.
+  Compiled Mon 10-Apr-17 13:02 by mcpre""",
+
+  """Cisco IOS XE Software, Version BLD_V163_MR_THROTTLE_LATEST_20170410_093453_V16_3_3_24 Cisco IOS Software [Denali], ASR1000 Software (X86_64_LINUX_IOSD-UNIVERSALK9-M), Experimental Version 16.3.20170410:103306 [v163_mr_throttle-BLD-BLD_V163_MR_THROTTLE_LATEST_20170410_093453 118]
+  Copyright (c) 1986-2017 by Cisco Systems, Inc.
+  Compiled Mon 10-Apr-17 04:35 by mcpre""",
+
+  """Cisco IOS XE Software, Version 16.06.05 Cisco IOS Software [Everest], ISR Software (X86_64_LINUX_IOSD-UNIVERSALK9-M), Version 16.6.5, RELEASE SOFTWARE (fc3)
+  Technical Support: http://www.cisco.com/techsupport
+  Copyright (c) 1986-2018 by Cisco Systems, Inc.
+  Compiled Mon 10-Dec-18 13:10 by mcpre""",
+
+  """Cisco IOS Software, IOS-XE Software, Catalyst 4500 L3 Switch Software (cat4500e-UNIVERSALK9-M), Version 03.03.02.SG RELEASE SOFTWARE (fc1)
+  Technical Support: http://www.cisco.com/techsupport
+  Copyright (c) 1986-2012 by Cisco Systems, Inc.
+  Compiled Tue 23-Oct-12 23:51 by prod_rel_team""",
+
+  """Cisco IOS Software, IOS-XE Software, Catalyst 4500 L3 Switch  Software (cat4500e-UNIVERSALK9-M), Version 03.04.06.SG RELEASE SOFTWARE (fc1) Technical Support: http://www.cisco.com/techsupport
+  Copyright (c) 1986-2015 by Cisco Systems, Inc.
+  Compiled Mon 04-May-15 02:44 by prod_rel_team""",
+
+  """Cisco IOS Software, IOS-XE Software, Catalyst 4500 L3 Switch Software (cat4500e-UNIVERSALK9-M), Version 03.08.06.E RELEASE SOFTWARE (fc4)
+  Technical Support: http://www.cisco.com/techsupport
+  Copyright (c) 1986-2018 by Cisco Systems, Inc.
+  Compiled Thu 05-Apr-18 06:32 by prod_rel_team""",
+
+  """Cisco IOS Software [Denali], Catalyst L3 Switch Software (CAT3K_CAA-UNIVERSALK9-M), Version 16.3.1a, RELEASE SOFTWARE (fc4)
+  Technical Support: http://www.cisco.com/techsupport
+  Copyright (c) 1986-2016 by Cisco Systems, Inc.
+  Compiled Thu 29-Sep-16 22:08 by mcpre""",
+
+  "Cisco IOS Software [Fuji], ASR1000 Software (X86_64_LINUX_IOSD-UNIVERSALK9-M), Version 16.7.1prd4, RELEASE SOFTWARE (fc1)",
+  "Cisco IOS Software [Fuji], Catalyst L3 Switch Software (CAT3K_CAA-UNIVERSALK9-M), Experimental Version 16.8.20170924:182909 [polaris_dev-/nobackup/mcpre/BLD-BLD_POLARIS_DEV_LATEST_20170924_191550 132]",
+  """Cisco IOS Software, IOS-XE Software, Catalyst 4500 L3 Switch  Software (cat4500e-UNIVERSALK9-M), Version 03.08.06.E RELEASE SOFTWARE (fc4)
+  Technical Support: http://www.cisco.com/techsupport
+  Copyright (c) 1986-2018 by Cisco Systems, Inc.
+  Compiled Thu 05-Apr-18 06:32 by prod_rel_team""",
+]
